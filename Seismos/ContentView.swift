8bbb022660f1012147f826ec8ee61fd4f4b3e924
Aanuoluwa Otitoola <aanuoluwa.otitoola@gmail.com>
2020-05-17 00:02:31 +0100

    Initial Commit

---
+++ Seismos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seismos
+//
+//  Created by Aanuoluwa Otitoola on 17/05/2020.
+//  Copyright © 2020 Aanuoluwa Otitoola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
